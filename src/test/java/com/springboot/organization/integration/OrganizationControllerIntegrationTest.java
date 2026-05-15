43cd02c447f4f9b1b24563f66611a90010d24dec
Snorri <snor0259@stud.kea.dk>
2026-05-15 18:53:30 +0200

    integration canGetAllOrganizations test

--- src/test/java/com/springboot/organization/integration/OrganizationControllerIntegrationTest.java
+++ src/test/java/com/springboot/organization/integration/OrganizationControllerIntegrationTest.java
@@ -2,6 +2,7 @@ package com.springboot.organization.integration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.springboot.organization.payload.OrganizationDTO;
+import com.springboot.organization.repository.OrganizationRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,8 +12,11 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -23,14 +27,18 @@ public class OrganizationControllerIntegrationTest {
     private MockMvc mockMvc; // fake browser
 
     @Autowired
+    private OrganizationRepository organizationRepository;
+
+    @Autowired
     private ObjectMapper objectMapper; // JAVA to JSON
 
     private OrganizationDTO organizationDTO;
 
     @BeforeEach
     void setUp() {
-        organizationDTO = new OrganizationDTO();
+        organizationRepository.deleteAll();
 
+        organizationDTO = new OrganizationDTO();
         organizationDTO.setName("Test Org");
         organizationDTO.setDescription("Test Description");
         organizationDTO.setLocation("Copenhagen");
@@ -41,7 +49,23 @@ public class OrganizationControllerIntegrationTest {
         mockMvc.perform(post("/api/organizations")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(organizationDTO)))
+                .andDo(print())
                 .andExpect(status().isCreated());
+    }
+    @Test
+    void canGetAllOrganizations() throws Exception {
+
+        // create organization
+        mockMvc.perform(post("/api/organizations")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(organizationDTO)));
+
+        // fetch organizations
+        mockMvc.perform(get("/api/organizations")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].name").value("Test Org"));
     }
 
 
